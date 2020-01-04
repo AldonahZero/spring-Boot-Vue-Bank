c22221413a548033bba0d84426f7fc4cfb3b5c8f
AldonahZero <i9980307@vip.qq.com>
2020-01-04 11:25:22 +0800

    kafka

--- bdemo/src/main/java/com/cx/bank/mhg/bdemo/BdemoApplication.java
+++ bdemo/src/main/java/com/cx/bank/mhg/bdemo/BdemoApplication.java
@@ -8,6 +8,19 @@ public class BdemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BdemoApplication.class, args);
+//		ConfigurableApplicationContext context = SpringApplication.run(BdemoApplication.class, args);
+//
+//		KafkaSender sender = context.getBean(KafkaSender.class);
+//
+//		for (int i = 0; i < 3; i++) {
+//			//调用消息发送类中的消息发送方法
+//			sender.send();
+//
+//			try {
+//				Thread.sleep(3000);
+//			} catch (InterruptedException e) {
+//				e.printStackTrace();
+//			}
+//		}
 	}
-
 }
